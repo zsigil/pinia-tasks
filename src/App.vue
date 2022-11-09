83d22a3d899ff33413675dfa53e3fbfe2ccfbf77
Zsigil <chickpox@freemail.hu>
2022-11-09 10:57:11 +0100

    adding header + css

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,13 @@
-<template></template>
+<template>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/logo.svg" alt="PInia logo" />
+      <h1>Pinia tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
