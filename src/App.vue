a23ad70beb192b75dbca1b750576cd314abb4ea7
Zsigil <chickpox@freemail.hu>
2022-11-21 10:16:01 +0100

    getters

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,22 @@
       <h1>Pinia tasks</h1>
     </header>
 
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Favorite tasks</button>
+    </nav>
     <!-- tasklist -->
-    <div class="task-list">
+    <div v-if="filter == 'all'" class="task-list">
+      <p>You have {{ taskStore.totalCount }} task(s) to do.</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <div v-if="filter == 'favs'" class="task-list">
+      <p>{{ taskStore.favCount }} favorite task(s) to do.</p>
+      <div v-for="task in taskStore.favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -16,6 +29,7 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import { useTaskStore } from "./stores/TaskStore";
 import TaskDetails from "./components/TaskDetails.vue";
 
@@ -23,7 +37,9 @@ export default {
   components: { TaskDetails },
   setup() {
     const taskStore = useTaskStore();
-    return { taskStore };
+
+    const filter = ref("all");
+    return { taskStore, filter };
   },
 };
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -6,6 +6,18 @@ export const useTaskStore = defineStore('taskStore', {
             { id: 1, title: "buy some milk", isFav: false },
             { id: 2, title: "play Gloomhaven", isFav: true },
         ],
-    })
+    }),
+    getters: {
+        favs() {
+            return this.tasks.filter(t => t.isFav)
+        },
+        favCount() {
+            return this.favs.length
+        },
+        //with arrow function, we cannot use 'this'
+        totalCount: (state) => {
+            return state.tasks.length
+        }
+    }
 
 })
